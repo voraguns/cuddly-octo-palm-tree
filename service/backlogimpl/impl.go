d4ae0bb82480ee489bf5f5b30e3df2b62f49c43d
voraguns <voragun2543@gmail.com>
2024-01-25 15:07:05 +0700

    add function in service layer

--- service/backlogimpl/impl.go
+++ service/backlogimpl/impl.go
@@ -1,7 +1,9 @@
 package backlogimpl
 
 import (
+	"context"
 	"cuddly-octo-palm-tree/config"
+	"cuddly-octo-palm-tree/domain"
 	bi_repo "cuddly-octo-palm-tree/repo/backlogimpl"
 
 	log "github.com/sirupsen/logrus"
@@ -21,4 +23,36 @@ func NewBiService(conf *config.Config, repo bi_repo.BiRepository) BiService {
 			"type": "service:Bi",
 		}),
 	}
+}
+
+func (s *BiServiceImpl) GetUsers(ctx context.Context) ([]*domain.Users, error) {
+	users, err := s.biRepo.GetUsers(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func (s *BiServiceImpl) GetOrders(ctx context.Context) ([]*domain.Orders, error) {
+	order, err := s.biRepo.GetOrders(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return order, nil
+}
+
+func (s *BiServiceImpl) GetOrderItems(ctx context.Context) ([]*domain.OrderItems, error) {
+	orderItem, err := s.biRepo.GetOrderItems(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return orderItem, nil
+}
+
+func (s *BiServiceImpl) GetProducts(ctx context.Context) ([]*domain.Products, error) {
+	products, err := s.biRepo.GetProducts(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
 }
--- service/backlogimpl/interface.go
+++ service/backlogimpl/interface.go
@@ -1,4 +1,13 @@
 package backlogimpl
 
+import (
+	"context"
+	"cuddly-octo-palm-tree/domain"
+)
+
 type BiService interface {
+	GetUsers(ctx context.Context) ([]*domain.Users, error)
+	GetOrders(ctx context.Context) ([]*domain.Orders, error)
+	GetOrderItems(ctx context.Context) ([]*domain.OrderItems, error)
+	GetProducts(ctx context.Context) ([]*domain.Products, error)
 }
