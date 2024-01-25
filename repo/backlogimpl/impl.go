fef508fe8cb40673ad486b3367d4ed807612a00a
voraguns <voragun2543@gmail.com>
2024-01-25 15:06:18 +0700

    add function in repo

--- repo/backlogimpl/impl.go
+++ repo/backlogimpl/impl.go
@@ -1,6 +1,11 @@
 package backlogimpl
 
-import "gorm.io/gorm"
+import (
+	"context"
+	"cuddly-octo-palm-tree/domain"
+
+	"gorm.io/gorm"
+)
 
 type BiRepositoryImpl struct {
 	db *gorm.DB
@@ -10,4 +15,36 @@ func NewBiRepository(db *gorm.DB) BiRepository {
 	return &BiRepositoryImpl{
 		db: db,
 	}
+}
+
+func (r *BiRepositoryImpl) GetUsers(ctx context.Context) ([]*domain.Users, error) {
+	var users []*domain.Users
+	if err := r.db.Find(&users).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func (r *BiRepositoryImpl) GetProducts(ctx context.Context) ([]*domain.Products, error) {
+	var products []*domain.Products
+	if err := r.db.Find(&products).Error; err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+func (r *BiRepositoryImpl) GetOrderItems(ctx context.Context) ([]*domain.OrderItems, error) {
+	var orderItem []*domain.OrderItems
+	if err := r.db.Find(&orderItem).Error; err != nil {
+		return nil, err
+	}
+	return orderItem, nil
+}
+
+func (r *BiRepositoryImpl) GetOrders(ctx context.Context) ([]*domain.Orders, error) {
+	var orders []*domain.Orders
+	if err := r.db.Find(&orders).Error; err != nil {
+		return nil, err
+	}
+	return orders, nil
 }
--- repo/backlogimpl/interface.go
+++ repo/backlogimpl/interface.go
@@ -1,4 +1,13 @@
 package backlogimpl
 
+import (
+	"context"
+	"cuddly-octo-palm-tree/domain"
+)
+
 type BiRepository interface {
+	GetUsers(ctx context.Context) ([]*domain.Users, error)
+	GetProducts(ctx context.Context) ([]*domain.Products, error)
+	GetOrderItems(ctx context.Context) ([]*domain.OrderItems, error)
+	GetOrders(ctx context.Context) ([]*domain.Orders, error)
 }
